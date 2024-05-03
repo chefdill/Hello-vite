979bc4cfac7e28bb8b149861507867f7a9576a2f
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-05-02 19:00:37 -0500

    done with self-practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import  Header from'./Header'
 import Main from './Main'
+import Animal from './Animals'
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Main />
+      <Animal selectedAnimal="duck"/>
       </div>
 
   )
 }
 
-export default App
+export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const data = [
+    { 
+        name: "armadillo",
+        image: new URL("../assets/animals/armadillo.jpg", import.meta.url).href
+    },
+    { 
+        name: "duck",
+        image: new URL("../assets/animals/duck.jpg", import.meta.url).href
+    },
+    { 
+        name: "default",
+        image: new URL("../assets/animals/default.jpg", import.meta.url).href
+    },
+    
+]
